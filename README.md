08ce277ffaf008b6fb73a468d5468e70b54c8c77
yuri-cinesi <yuri.cinesi@gmail.com>
2020-08-28 17:51:38 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,21 @@
+# DestreamerChannelDL
+
+Very simple script to bulk download videos from microsoft stream channel.
+
+Requirements:
+.NET core runtime, https://dotnet.microsoft.com/download
+
+Preparation:
+1) Open up the channel you'd like to download videos from
+2) Scroll to the bottom to load all the videos
+3) Right click > inspect > copy the body and save it to a file somewhere
+
+Usage:
+streamchannelDL.exe "path-to-file" "path-to-dstreamer.cmd" "output-dir" [--threads X]
+The threads setting is optional, it lets you parallelize the download (but in a very janky way that's essentially just running multiple destreamer instances).
+
+I made this in like an afternoon, might have bugs idk. The "isdev" check simply autopopulates it with my own settings because I'm lazy AF.
+
+
+
+Uh. That's about it. Why are you still here? Leave.
---
+++ StreamVideoExtract/Program.vb
@@ -0,0 +1,133 @@
+Imports System
+Imports System.IO
+
+Module Program
+    Dim ParentDir As String = Environment.CurrentDirectory
+    Sub Main(args As String())
+        Dim args2 As List(Of String) = args.ToList
+        Dim threads As Integer = 1
+
+        If File.Exists(ParentDir & "\isdev.txt") Then    'Autopopulates it for my specific setup because I'm lazy AF lmao
+            Dim di As New DirectoryInfo(ParentDir)
+            For Each f As FileInfo In di.GetFiles
+                If f.Name.Contains("data-") Then
+                    args2.Insert(0, f.FullName)
+                    args2.Insert(1, """" & Environment.GetFolderPath(Environment.SpecialFolder.Desktop) & "\Programs\Destreamer\destreamer\destreamer.cmd""")
+                    Console.WriteLine(args2(1))
+                    Console.ReadLine()
+                    args2.Insert(2, """I:\Lezioni\" & f.Name.Replace("data-", "").Replace(".txt", "") & """")
+                End If
+            Next
+
+        End If
+
+        If args2.Count < 3 Then
+            Console.WriteLine("Usage: streamchannelDL.exe ""path-to-file"" ""path-to-destreamer.cmd"" ""output-dir"" [--threads X]" & vbCrLf)
+            Console.WriteLine("The stream site is weird and I'm too dumb to figure out how to load the entire" & vbCrLf & "list of videos, so you'll have to do it yourself. Just scroll to the bottom, do inspect, copy the body and save it to a file.")
+            Console.WriteLine("You can also optionally add --threads X to parallelize it")
+            Return
+        End If
+
+        Dim i As Integer
+
+        For i = 0 To args.Length - 1
+            If args(i) = "--threads" Then
+                If args.Length > i + 1 Then
+                    threads = args(i + 1)
+                End If
+            End If
+        Next
+
+
+        If args2(0) = "" Or args2(1) = "" Or args2(2) = "" Then
+            Console.WriteLine("Usage: streamchannelDL.exe ""path-to-file"" ""path-to-destreamer.cmd"" ""output-dir""")
+            Console.WriteLine("The stream site is weird and I'm too dumb to figure out how to load the entire list of videos,_
+             so you'll have to do it yourself. Just scroll to the bottom, do inspect, copy the body and save it to a file.")
+        End If
+
+        For Each s In args2
+            s.Replace("""", "")
+        Next
+
+        Dim text As String = ""
+
+        Try
+            text = File.ReadAllText(args2(0))  'Why yes this is a hackjob done in five minutes, why do you ask?
+        Catch ex As Exception
+            Console.WriteLine("Error reading file.")
+            Console.ReadLine()
+            Console.WriteLine(ex.ToString)
+            Console.ReadLine()
+        End Try
+
+        Dim URLs As New List(Of String)
+        i = text.IndexOf(" href=""/video/")
+        Dim i2 As Integer
+
+        Do Until i = -1
+            i = text.IndexOf("""", i)
+            i2 = text.IndexOf("?", i + 1)
+            Console.WriteLine(text.Substring(i + 1, i2 - i - 1))
+            If URLs.Count > 0 Then
+                If URLs(URLs.Count - 1) <> ("https://web.microsoftstream.com" & text.Substring(i + 1, i2 - i - 1)) Then 'Avoid repeat elements
+                    URLs.Add("https://web.microsoftstream.com" & text.Substring(i + 1, i2 - i - 1))
+                End If
+            Else
+                URLs.Add("https://web.microsoftstream.com" & text.Substring(i + 1, i2 - i - 1))
+            End If
+
+            i = text.IndexOf(" href=""/video/", i)
+        Loop
+
+
+        If File.Exists(ParentDir & "\urls0.txt") Then
+            For i = 0 To threads
+                If File.Exists(ParentDir & "\urls" & i & ".txt") Then
+                    File.Delete(ParentDir & "\urls" & i & ".txt")
+                End If
+            Next
+        End If
+
+        For Each s As String In URLs
+            File.AppendAllText(ParentDir & "\urls" & (URLs.IndexOf(s) Mod threads + 1) & ".txt", s & vbCrLf)
+        Next
+        For i = 0 To threads
+            If File.Exists(ParentDir & "\urls" & i & ".txt") Then
+                RunCommandH(args2(1), " -f """ & ParentDir & "\urls" & i & ".txt"" -k -o """ & args2(2) & """ -x -v --format mp4 --skip")
+            End If
+        Next
+
+    End Sub
+
+
+    Function RunCommandH(Command As String, Arguments As String) As String
+        Console.WriteLine(Command & Arguments)
+        'Console.ReadLine()
+        Dim oProcess As New Process()
+        Dim oStartInfo As New ProcessStartInfo(Command, Arguments)
+        oStartInfo.WorkingDirectory = Command.Substring(0, Command.LastIndexOf("\"))
+        Console.WriteLine(oStartInfo.WorkingDirectory)
+        Console.ReadLine()
+        oStartInfo.UseShellExecute = True
+        oStartInfo.RedirectStandardOutput = False
+        oProcess.StartInfo = oStartInfo
+        Try
+            oProcess.Start()
+        Catch ex As Exception
+            File.WriteAllText(ParentDir & "crashreport.txt", ex.ToString)
+            Console.WriteLine(ParentDir)
+            Console.WriteLine(ex.ToString)
+            Console.WriteLine("Something went wrong.")
+            Return "Error"
+            Console.ReadLine()
+        End Try
+
+
+
+        'oProcess.WaitForExit()
+
+        'oProcess.Dispose()
+
+        Return ""
+    End Function
+End Module
